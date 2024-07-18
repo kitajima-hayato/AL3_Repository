cfa55662a4969e7dde321bccd4a3bf41d507d66c
kitajima-hayato <k023g0057@g.neec.ac.jp>
2024-07-18 10:04:12 +0900

    color_.wをマイフレーム減算実装 clampで上限実装

--- DirectXGame/DeathParticles.cpp
+++ DirectXGame/DeathParticles.cpp
@@ -43,8 +43,8 @@ void DeathParticles::Update() {
 		// 終了扱いにする
 		isFinished_ = true;
 	}
-
-	color_.w = std::clamp(1.0f, 1.0f, 0.0f, 1.0f);
+	color_.w -= 0.01f;
+	color_.w = std::clamp(color_.w, 0.0f, 1.0f);
 	// 色変更オブジェクトに色の数値を設定する
 	objectColor_.SetColor(color_);
 	// 色変更オブジェクトをVRAMに転送
